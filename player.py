2309a360717be4beb84505093410ccb742d74cc7
sanderhiemstra <sander.hiemstra@gmail.com>
2025-07-15 02:44:50 +0200

    Added player rotation

--- player.py
+++ player.py
@@ -3,12 +3,11 @@ from circleshape import CircleShape
 from constants import *
 
 
-
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-    
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
@@ -19,3 +18,14 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
